d54ba0345784fba4529f3df5a94aa288057a5414
Phileas Fogg <phileas.fogg@mail.com>
2020-09-27 00:09:36 +0200

    Write compiled shaders to different directories

--- CompileShaders.sh
+++ CompileShaders.sh
@@ -37,42 +37,43 @@ includepath="bgfx/bgfx/src"
 allfsfiles="`find SDLBgfxTest/shaders -name *.fs`"
 for fsfile in $allfsfiles
 do
-    basename="`echo $fsfile | cut -f 1 -d '.'`"
-    vsfile="${basename}.vs"
-    fsbin="build/${fsfile}.bin"
-    vsbin="build/${vsfile}.bin"
+    namewoext="`echo $fsfile | cut -f 1 -d '.'`"
+    vsfile="${namewoext}.vs"
+    basename="`basename $fsfile`"
+    basenamewoext="`echo $basename | cut -f 1 -d '.'`"
 
     #TODO: platform asm.js ?
 
     # DX9
-    ${path_shaderc} -f "${vsfile}" -o "${vsbin}" --platform windows -p vs_3_0 -O 3 --type vertex --verbose -i $includepath
-    ${path_shaderc} -f "${fsfile}" -o "${fsbin}" --platform windows -p ps_3_0 -O 3 --type fragment --verbose -i $includepath
+    ${path_shaderc} -f "${vsfile}" -o "build/SDLBgfxTest/shaders/dx9/${basenamewoext}.vs.bin" "${vsbin}" --platform windows -p vs_3_0 -O 3 --type vertex --verbose -i $includepath
+    ${path_shaderc} -f "${fsfile}" -o "build/SDLBgfxTest/shaders/dx9/${basenamewoext}.fs.bin" --platform windows -p ps_3_0 -O 3 --type fragment --verbose -i $includepath
 
     # DX11
-    ${path_shaderc} -f "${vsfile}" -o "${vsbin}" --platform windows -p vs_5_0 -O 3 --type vertex --verbose -i $includepath
-    ${path_shaderc} -f "${fsfile}" -o "${fsbin}" --platform windows -p ps_5_0 -O 3 --type fragment --verbose -i $includepath
+    ${path_shaderc} -f "${vsfile}" -o "build/SDLBgfxTest/shaders/dx11/${basenamewoext}.vs.bin" --platform windows -p vs_5_0 -O 3 --type vertex --verbose -i $includepath
+    ${path_shaderc} -f "${fsfile}" -o "build/SDLBgfxTest/shaders/dx11/${basenamewoext}.fs.bin" --platform windows -p ps_5_0 -O 3 --type fragment --verbose -i $includepath
 
     # NACL
-    ${path_shaderc} -f "${vsfile}" -o "${vsbin}" --platform nacl --type vertex --verbose -i $includepath
-    ${path_shaderc} -f "${fsfile}" -o "${fsbin}" --platform nacl --type fragment --verbose -i $includepath
+    ${path_shaderc} -f "${vsfile}" -o "build/SDLBgfxTest/shaders/essl/${basenamewoext}.vs.bin" --platform nacl --type vertex --verbose -i $includepath
+    ${path_shaderc} -f "${fsfile}" -o "build/SDLBgfxTest/shaders/essl/${basenamewoext}.fs.bin" --platform nacl --type fragment --verbose -i $includepath
 
     # Android
-    ${path_shaderc} -f "${vsfile}" -o "${vsbin}" --platform android --type vertex --verbose -i $includepath
-    ${path_shaderc} -f "${fsfile}" -o "${fsbin}" --platform android --type fragment --verbose -i $includepath
+    ${path_shaderc} -f "${vsfile}" -o "build/SDLBgfxTest/shaders/essl/${basenamewoext}.vs.bin" --platform android --type vertex --verbose -i $includepath
+    ${path_shaderc} -f "${fsfile}" -o "build/SDLBgfxTest/shaders/essl/${basenamewoext}.fs.bin" --platform android --type fragment --verbose -i $includepath
 
     # GLSL
-    ${path_shaderc} -f "${vsfile}" -o "${vsbin}" --platform linux -p 120 --type vertex --verbose -i $includepath
-    ${path_shaderc} -f "${fsfile}" -o "${fsbin}" --platform linux -p 120 --type fragment --verbose -i $includepath
+    ${path_shaderc} -f "${vsfile}" -o "build/SDLBgfxTest/shaders/glsl/${basenamewoext}.vs.bin" --platform linux -p 120 --type vertex --verbose -i $includepath
+    ${path_shaderc} -f "${fsfile}" -o "build/SDLBgfxTest/shaders/glsl/${basenamewoext}.fs.bin" --platform linux -p 120 --type fragment --verbose -i $includepath
 
     # Metal
-    ${path_shaderc} -f "${vsfile}" -o "${vsbin}" --platform osx -p metal --type vertex --verbose -i $includepath
-    ${path_shaderc} -f "${fsfile}" -o "${fsbin}" --platform osx -p metal --type fragment --verbose -i $includepath
+    ${path_shaderc} -f "${vsfile}" -o "build/SDLBgfxTest/shaders/metal/${basenamewoext}.vs.bin" --platform osx -p metal --type vertex --verbose -i $includepath
+    ${path_shaderc} -f "${fsfile}" -o "build/SDLBgfxTest/shaders/metal/${basenamewoext}.fs.bin" --platform osx -p metal --type fragment --verbose -i $includepath
 
     # PSSL
-    ${path_shaderc} -f "${vsfile}" -o "${vsbin}" --platform orbis -p pssl --type vertex --verbose -i $includepath
-    ${path_shaderc} -f "${fsfile}" -o "${fsbin}" --platform orbis -p pssl --type fragment --verbose -i $includepath
+    ${path_shaderc} -f "${vsfile}" -o "build/SDLBgfxTest/shaders/pssl/${basenamewoext}.vs.bin" --platform orbis -p pssl --type vertex --verbose -i $includepath
+    ${path_shaderc} -f "${fsfile}" -o "build/SDLBgfxTest/shaders/pssl/${basenamewoext}.fs.bin" --platform orbis -p pssl --type fragment --verbose -i $includepath
 
     # Spirv
-    ${path_shaderc} -f "${vsfile}" -o "${vsbin}" --platform linux -p spirv --type vertex --verbose -i $includepath
-    ${path_shaderc} -f "${fsfile}" -o "${fsbin}" --platform linux -p spirv --type fragment --verbose -i $includepath
+    ${path_shaderc} -f "${vsfile}" -o "build/SDLBgfxTest/shaders/spirv/${basenamewoext}.vs.bin" --platform linux -p spirv --type vertex --verbose -i $includepath
+    ${path_shaderc} -f "${fsfile}" -o "build/SDLBgfxTest/shaders/spirv/${basenamewoext}.fs.bin" --platform linux -p spirv --type fragment --verbose -i $includepath
 done
+
